0a0c3bbcc751595e13d1cc544640901905bf4c4d
Karina <108223541+KarinaKazamanova@users.noreply.github.com>
2023-04-21 17:32:26 +0300

    Add files via upload

---
+++ DZ2/DZ2_4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DZ2_4 {
+    public static void main(String[] args) throws Exception {
+        try {
+            noempty();
+        } catch (emptyStringException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    private static void noempty() throws Exception {
+        Scanner s = new Scanner(System.in);
+        String inputString = s.nextLine();
+        if (inputString.length() == 0) {
+            throw new emptyStringException();
+        } else {
+            System.out.println("Все ок!");
+        }
+        s.close();
+    }
+}
